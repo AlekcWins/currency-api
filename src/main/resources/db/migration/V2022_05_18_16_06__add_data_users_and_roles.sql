8cf966858b0e1f92c3b3364f3b852bbeb20e1399
Alexandr <aleksand2000@gmail.com>
2022-05-30 14:09:44 +0400

    deploy : prepare project to deploy add environment variables

--- src/main/resources/db/migration/V2022_05_18_16_06__add_data_users_and_roles.sql
+++ src/main/resources/db/migration/V2022_05_18_16_06__add_data_users_and_roles.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.users ( password, username)
-VALUES ('$2a$10$3YAyP9FIdOsdFU3SlflYtObiXwL9XpjMYgQVWg7GOECrpV.4L4r52', 'admin');
+VALUES ('$2a$12$wNp5gLbP2wBzi1Hni12EBuHV5uQktLKEZLLSyfxq7YlFBuLo6v.MK', 'admin');
 
 
 INSERT INTO public.user_role (user_id, role) VALUES (1, 'USER');
